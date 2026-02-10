cef68d83fa1b28dc3cc76e462400a7d09e873197
shokh0126mardonov <smardonov203@gmail.com>
2026-02-10 18:30:45 +0500

    done

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater,CommandHandler,MessageHandler
 
-def start(update, context):
-    update.message.reply_text("Bot ishlayapti")
+from handlers import start
+from utils import settings
 
-updater = Updater("8301674119:AAGQqrjGkLI4w02hRNt9oRxTEg5NB1wBELM", use_context=True)
-dp = updater.dispatcher
-dp.add_handler(CommandHandler("start", start))
+def main():
+    updater = Updater(settings.TOKEN)
+    dispatcher = updater.dispatcher
 
-updater.start_polling()
-updater.idle()
+    dispatcher.add_handler(CommandHandler('start',start))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
---
+++ utils/config.py
@@ -0,0 +1,16 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Data:
+    DB_NAME = os.getenv('DB_NAME')
+    DB_PASS = os.getenv('DB_PASS')
+    DB_PORT = os.getenv('DB_PORT')
+    DB_HOST = os.getenv('DB_HOST')
+    DB_USER = os.getenv('DB_USER')
+    TOKEN   = os.getenv('TOKEN')
+
+
+settings = Data()
